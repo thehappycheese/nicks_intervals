944f65158e79528a953416cb11e8172a0c1b3fb6
thehappycheese <the.nicholas.archer@gmail.com>
2020-12-02 00:19:36 +0800

    I learned about deriving equality for enums

--- src/bound.rs
+++ src/bound.rs
@@ -1,7 +1,7 @@
 use std::cmp;
 use std::ops;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum BoundDirection{
 	PartOfLeft,
 	PartOfRight
@@ -17,6 +17,19 @@ pub enum Bound{
 	PositiveInfinity
 }
 
-impl ops:: for Bound{
-	
+impl cmp::PartialEq<Bound> for Bound{
+	fn eq(&self, other: &Self) -> bool{
+		match other{
+			Bound::NegativeInfinity => {self == &Bound::NegativeInfinity}
+			Bound::PositiveInfinity => {self == &Bound::PositiveInfinity}
+			Bound::FiniteBound{value:other_value, direction:other_direction} =>{
+				if let Bound::FiniteBound{value: self_value,direction:self_direction} = self {
+					// TODO: This comparison is fraught with floating point problems:
+					other_value == self_value && other_direction == self_direction
+				}else{
+					false
+				}
+			}
+		}
+	}
 }

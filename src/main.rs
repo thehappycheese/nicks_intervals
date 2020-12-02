351a885b15d0f875f601a9e08465024ed621f8a9
thehappycheese <the.nicholas.archer@gmail.com>
2020-12-02 23:41:27 +0800

    added some tests but something is fishy

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,30 @@ mod bound;
 extern crate float_cmp;
 
 use interval::{Interval};
+use bound::{Bound, BoundDirection};
+use BoundDirection::{PartOfRight, PartOfLeft};
 
 
 fn main(){
 	let i = Interval::closed_inf(5.0);
-	println!("{:?}", i)
+	let a = Bound::FiniteBound{value:10.0,direction:PartOfRight};
+	let b = Bound::FiniteBound{value:10.0,direction:PartOfLeft};
+
+	println!("{:?}", a>b)
+}
+
+#[test]
+fn test_bound_order(){
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfLeft	}	<	Bound::FiniteBound{value:0.0, direction:PartOfRight	},	false);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfLeft	}	<	Bound::FiniteBound{value:0.0, direction:PartOfLeft	},	false);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfRight	}	<	Bound::FiniteBound{value:0.0, direction:PartOfRight	},	false);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfRight	}	<	Bound::FiniteBound{value:0.0, direction:PartOfLeft	},	true);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfLeft	}	==	Bound::FiniteBound{value:0.0, direction:PartOfRight	},	false);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfLeft	}	==	Bound::FiniteBound{value:0.0, direction:PartOfLeft	},	true);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfRight	}	==	Bound::FiniteBound{value:0.0, direction:PartOfRight	},	true);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfRight	}	==	Bound::FiniteBound{value:0.0, direction:PartOfLeft	},	false);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfLeft	}	>	Bound::FiniteBound{value:0.0, direction:PartOfRight	},	true);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfLeft	}	>	Bound::FiniteBound{value:0.0, direction:PartOfLeft	},	false);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfRight	}	>	Bound::FiniteBound{value:0.0, direction:PartOfRight	},	false);
+	assert_eq!(Bound::FiniteBound{value:0.0, direction:PartOfRight	}	>	Bound::FiniteBound{value:0.0, direction:PartOfLeft	},	false);
 }
